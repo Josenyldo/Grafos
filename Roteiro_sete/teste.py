2681b726cf4c3b4b8d8e397f9fc9f5f56b1ed9e3
josenyldo <josenildo.simao@gmail.com>
2019-05-28 22:31:18 -0300

    A DFS está incompleta. Salvando por segurança.

---
+++ Roteiro_sete/teste.py
@@ -0,0 +1,8 @@
+from Roteiro_sete.grafo_adj_nao_dir import *
+
+g = Grafo()
+for i in ['A', 'B', 'C', 'D']:
+    g.adiciona_vertice(i)
+for i in ['A-B', 'A-C', 'D-C', 'B-C', 'B-D']:
+    g.adiciona_aresta(i)
+print(g)
